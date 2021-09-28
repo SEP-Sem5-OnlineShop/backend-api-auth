053459b82b5d7821d102da6edfabf92920acdd9d
Darshana Sandaruwan <darshana.18@cse.mrt.ac.lk>
2021-09-28 11:09:48 +0530

    slight changes

--- routes/app/driver.js
+++ routes/app/driver.js
@@ -4,5 +4,7 @@ const tokenHandler = require('../../utils/tokenHenadler')
 
 router.post('/driver', DriverController.create)
 router.put('/driver', DriverController.update)
+router.get('/driver/image', DriverController.getImage)
+router.put('/driver/image', DriverController.updateImage)
 
 module.exports = router
