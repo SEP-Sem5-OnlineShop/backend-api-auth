37d49724b0afe23d8dcdf08951de1e900c2a145a
danukadj <danukasandaruwandj@gmail.com>
2021-10-14 14:33:42 +0530

    vendor rating

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const Product = require("../database/schemas/productSchema")
 const User = require("../database/schemas/userSchema")
 const {mongoose} = require("../database/connection")
+const Vendor = require('../models/vendor');
 
 module.exports.create = async (data) => {
     let session;
@@ -82,71 +83,18 @@ module.exports.getMaxProducts = async () => {
 
 
 module.exports.getVendorProductList = async (id) => {
-    // const createdProducts = await Product.insertMany([
-    //     {
-    //         product_name: 'Burger with Fries',
-    //         seller: '613eb365af0d5b2c142fa326',
-    //         imageThumbnailUrl: '/img/item1.png',
-    //         imageUrl: '/img/item1.png',
-    //         description: 'product description',
-    //         price: 100,
-    //         stock: 10,
-    //         status: 'available',
-    //         rating: 4.5,
-    //         numReviews: 2,
-    //         reviews: [
-    //             {
-    //                 rating: 4.0,
-    //                 review: 'good product',
-    //                 customer: '613ebc89c71d2e07e0ec5e93',
-    //             },
-    //             {
-    //                 rating: 5.0,
-    //                 review: 'good product',
-    //                 customer: '613ebc89c71d2e07e0ec5e93',
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         product_name: 'Burger with Fries',
-    //         seller: '613eb365af0d5b2c142fa326',
-    //         imageThumbnailUrl: '/img/item1.png',
-    //         imageUrl: '/img/item1.png',
-    //         description: 'product description',
-    //         price: 100,
-    //         stock: 0,
-    //         status: 'available',
-    //     },
-    //     {
-    //         product_name: 'Burger with Fries',
-    //         seller: '613eb365af0d5b2c142fa326',
-    //         imageThumbnailUrl: '/img/item1.png',
-    //         imageUrl: '/img/item1.png',
-    //         description: 'product description',
-    //         price: 100,
-    //         stock: 10,
-    //         status: "available",
-    //         rating: 4.5,
-    //         numReviews: 1,
-    //         reviews: [
-    //             {
-    //                 rating: 4.0,
-    //                 review: 'good product',
-    //                 customer: '613ebc89c71d2e07e0ec5e93',
-    //             }
-    //         ],
-    //     }
-    // ]);
     return Product.find({seller: id});
 }
 
 module.exports.getVendorSellProductList = async (id) => {
     return Product.find({ seller:id, stock: { $gt: 0 } });
 }
+
 module.exports.addReview = async (product_id,review) => {
     const prod = await module.exports.getProduct(product_id);
     const newNumReviews = prod.numReviews + 1;
     const newRating = (prod.rating*prod.numReviews + review.rating)/ newNumReviews ;
+    const vendor = await Vendor.addRating(prod.seller,review.rating);
     return Product.updateOne(
         { "_id": product_id},
         { 
