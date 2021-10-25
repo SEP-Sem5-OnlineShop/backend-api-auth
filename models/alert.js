d8208ea0e0f097da160f6e7995e744b36f32a447
danukadj <danukasandaruwandj@gmail.com>
2021-10-25 20:19:23 +0530

    alert data model test over

--- models/alert.js
+++ models/alert.js
@@ -1,26 +1,21 @@
-const bcrypt = require("bcrypt")
-
-const User = require("../database/schemas/alertSchema")
 const Alert = require("../database/schemas/alertSchema")
 
 /**
  * Create a alert
  * @param data
  */
-module.exports.createAlert = async () => {
-        const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(data.password, salt)
-        return new User({
-                user_id: "abc12345",
-                product_id:"bcd123",
-                status: "sent",
-                setTime: "2020-08-31 12:00",
-                updateTime: "2020-08-31 12:02",
+// module.exports.createAlert = async () => {
+//         const salt = await bcrypt.genSalt(10)
+//         const hashPassword = await bcrypt.hash(data.password, salt)
+//         return new User({
+//                 user_id: "abc12345",
+//                 product_id:"bcd123",
+//                 status: "sent",
+//                 setTime: "2020-08-31 12:00",
+//                 updateTime: "2020-08-31 12:02",
                 
-        });
-}
-
-// get a alert
+//         });
+// }
 
 
 module.exports.setAlert = async (customer_id,product_id) => {
--- tests/unitTests/model/alert.test.js
+++ tests/unitTests/model/alert.test.js
@@ -1,7 +1,6 @@
 const mockingoose = require('mockingoose');
 const AlertSchema = require("../../../database/schemas/alertSchema")
 const {
-    createAlert,
     setAlert,
     removeAlert,
     getdetailsAlert,
@@ -11,6 +10,35 @@ const {
 
 describe('alert', () => {
 
+    describe('setAlert', () => {
+        it ('should return relevant user', async () => {
+            mockingoose(AlertSchema).toReturn(
+                {
+                user_id: '614f6ab86728ee0a9450195c',
+                product_id: '6152e3cca58556299c756046',
+                created_at:'2021-09-22T10:55:31.953+00:00',
+                updated_at:'2021-09-26T18:02:13.379+00:00',
+                }, 'create');
+            const results = await setAlert('614f6ab86728ee0a9450195c','6152e3cca58556299c756046');
+            expect(results.product_id.toString()).toEqual('6152e3cca58556299c756046');
+        });
+    });
+
+    describe('removeAlert', () => {
+        it ('should return relevant user', async () => {
+            mockingoose(AlertSchema).toReturn(
+                {
+                _id: '6176bb506790883b30293be2',
+                user_id: '614f6ab86728ee0a9450195c',
+                product_id: '6152e3cca58556299c756046',
+                created_at:'2021-09-22T10:55:31.953+00:00',
+                updated_at:'2021-09-26T18:02:13.379+00:00',
+                }, 'deleteOne');
+            const results = await removeAlert('614f6ab86728ee0a9450195c','6152e3cca58556299c756046');
+            expect(results._id.toString()).toEqual('6176bb506790883b30293be2');
+        });
+    });
+    
     describe('getdetailsAlert', () => {
         it ('should return relevant user', async () => {
             mockingoose(AlertSchema).toReturn(
