8f39b85868f008df2de511e3a577e181caff0b14
Darsahan Sandaruwan <dsandaruwan7@gmail.com>
2021-11-08 13:28:59 +0530

    corsOptions method changed in app.js

--- app.js
+++ app.js
@@ -11,20 +11,28 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
+const allowList = ['http://localhost:3000', 'https://ontheway-sep.netlify.app', 'https://admin-ontheway-sep.netlify.app']
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         console.log(origin)
+//         if (allowList.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     },
+//     credentials: true
+// }
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (['http://localhost:3000', 'https://ontheway-sep.netlify.app', 'https://admin-ontheway-sep.netlify.app'].indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true
+const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    if (allowList.indexOf(req.header('Origin')) !== -1) corsOptions = {origin: true, credentials: true}
+    else corsOptions = {origin: false}
+    callback(null, corsOptions)
 }
 
 // app.use(cors())
-app.use(cors(corsOptions))
+app.use(cors(corsOptionsDelegate))
 
 app.use('/api', [apiRoutes, appRoutes])
 
