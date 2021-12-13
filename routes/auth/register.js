7fd3be9b8fc15c2995460d2ba551e3811ff2408d
D-sandaruwan <dsandaruwan7@gmail.com>
2021-12-13 13:53:51 +0530

    common register feature updated in routes/auth/regisetr.js

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -7,17 +7,29 @@ const User = require("../../models/user")
  * @returns {Promise<*>}
  */
 module.exports = async (req, res) => {
-    const user = await User.createUser(req.body)
     try {
-        res.status(201).send({
-            message: "User creation is successful!",
-            data:await user.save()
+        const userByTelephone = await User.getUserByTelephone(req.body.telephone)
+        if(!userByTelephone) {
+            const user = await User.createUser(req.body)
+            await user.save()
+            return res.status(201).send({
+                message: "Successful",
+                data: {
+                    user:user,
+                    message: "User creation is successful!"
+                }
+            })
+        }
+        return res.status(400).send({
+            message: "Failed",
+            data:"Given telephone number is already registered!"
         })
     }
     catch (error) {
-        return res.status(400).send({
-            message: "Bad Request!",
-            error: error
+        console.log(error)
+        return res.status(500).send({
+            message: "Failed",
+            data: "Something went wrong!"
         })
     }
 
